63b8499eb3f140aaa61dd2afe8abd0743595fa69
Amogh Dhavalikar <amoghdhavalikar@gmail.com>
2020-09-30 13:10:24 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.08,
+          'friction':13,
+          'density':0.05
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x,pos.y, this.width, this.height);
+    }
+  };
+  
